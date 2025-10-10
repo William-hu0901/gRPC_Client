417428cedb4295abf41bb5e755bc98369f8508ae
William-hu0901 <delong201314@gmail.com>
2025-10-10 16:58:12 +0800

    add initial code for grpc client

---
+++ src/main/java/net/daodao/grpc/DataQueryServiceClient.java
@@ -0,0 +1,23 @@
+package net.daodao.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class DataQueryServiceClient {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052)
+                .usePlaintext()
+                .build();
+
+        DataQueryServiceGrpc.DataQueryServiceBlockingStub stub = DataQueryServiceGrpc.newBlockingStub(channel);
+
+        DataQueryRequest request = DataQueryRequest.newBuilder()
+                .setQuery("SELECT * FROM users")
+                .build();
+
+        DataQueryResponse response = stub.queryData(request);
+        System.out.println("Response: " + response.getResult());
+
+        channel.shutdown();
+    }
+}
---
+++ src/test/java/net/daodao/grpc/DataQueryServiceClientTest.java
@@ -0,0 +1,39 @@
+package net.daodao.grpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DataQueryServiceClientTest {
+    private ManagedChannel channel;
+    private DataQueryServiceGrpc.DataQueryServiceBlockingStub stub;
+
+    @BeforeEach
+    public void setUp() {
+        channel = ManagedChannelBuilder.forAddress("localhost", 50052)
+                .usePlaintext()
+                .build();
+        stub = DataQueryServiceGrpc.newBlockingStub(channel);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (channel != null) {
+            channel.shutdown();
+        }
+    }
+
+    @Test
+    public void testClientQuery() {
+        DataQueryRequest request = DataQueryRequest.newBuilder()
+                .setQuery("SELECT * FROM users")
+                .build();
+
+        DataQueryResponse response = stub.queryData(request);
+        assertEquals("Processed query: SELECT * FROM users", response.getResult());
+    }
+}
